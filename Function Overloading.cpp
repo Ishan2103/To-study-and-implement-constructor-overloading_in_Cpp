2a7ba9d38ed8a095b5a41584392926a8e88e6d9f
Ishan2103 <ishanextra21@gmail.com>
2025-09-28 23:38:59 +0530

    Create Function Overloading.cpp

---
+++ Function Overloading.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+class Calculator {
+public:
+    int add(int a, int b) {
+        return a + b;
+    }
+    float add(float a, float b) {
+        return a + b;
+    }
+};
+int main() {
+    Calculator calc;
+    cout << "Integer Sum: " << calc.add(5, 10) << endl;
+    cout << "Float Sum: " << calc.add(5.5f, 2.5f) << endl;   
+
+    return 0;
+}
