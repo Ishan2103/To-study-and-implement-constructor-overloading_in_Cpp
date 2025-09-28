14d4278d3c4f2ee4b31800044acf8502c0e0c3bd
Ishan2103 <ishanextra21@gmail.com>
2025-09-28 23:38:18 +0530

    Create Addition using constructor overloading.cpp

---
+++ Addition using constructor overloading.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+class Sum {
+public:
+    // Constructor for two integers
+    Sum(int x, int y) {
+        cout << "Integer Sum (2 numbers) = " << (x + y) << endl;
+    }
+    Sum(float x, float y) {
+        cout << "Float Sum = " << (x + y) << endl;
+    }
+    Sum(int x, int y, int z) {
+        cout << "Integer Sum (3 numbers) = " << (x + y + z) << endl;
+    }
+};
+int main() {
+    int a, b, c;
+    float p, q;
+     cout << "Enter two integers: ";
+    cin >> a >> b;
+    cout << "Enter two floats: ";
+    cin >> p >> q;
+    cout << "Enter three integers: ";
+    cin >> a >> b >> c;
+    cout << "\nResults:" << endl;
+    Sum s1(a, b);      
+    Sum s2(p, q);      
+    Sum s3(a, b, c);   
+
+    return 0;
+}
